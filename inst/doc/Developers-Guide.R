ae404c14b9fbea7ad9a3c229eb92cd26a77fe650
Naeem Khoshnevis <nkhoshnevis@g.harvard.edu>
2023-09-29 23:52:39 +0000

    version 0.4.1

--- inst/doc/Developers-Guide.R
+++ inst/doc/Developers-Guide.R
@@ -1,4 +1,5 @@
 ## ---- include = FALSE---------------------------------------------------------
+options(rmarkdown.html_vignette.check_title = FALSE)
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
